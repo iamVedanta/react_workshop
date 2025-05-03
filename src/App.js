f87025f855ce41950d4d78975c626fa0c4dd90ab
iamVedanta <callvedanta@gmail.com>
2025-05-03 14:56:18 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Users from "./components/Users";
+import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import CreateForm from "./components/CreateForm";
+import { Link } from "react-router-dom";
 function App() {
   return (
-    <>
-      <CreateForm />
+    <Router>
+      <nav class="hi" className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Home
+          </Link>
+          <Link className="navbar-brand" to="/about">
+            About
+          </Link>
+          <Link className="navbar-brand" to="/create">
+            Create
+          </Link>
+        </div>
+      </nav>
       <Users />
-      
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<CreateForm />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div className="container mt-4">
+      <h2>About Us</h2>
+      <p>
+        Welcome to our website! We are dedicated to providing the best service
+        possible. Our team is committed to excellence and customer satisfaction.
+      </p>
+      <p>
+        If you have any questions or feedback, please feel free to contact us.
+      </p>
+    </div>
+  );
+}
